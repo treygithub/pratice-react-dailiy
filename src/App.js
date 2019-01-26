35e0dfba1780d41fb39934c132d4b0de05c92fda
robert hunnicutt <robhunn74@gmail.com>
2019-01-26 16:05:33 -0600

    init push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+     state = {
+    title: 'title of app',
+    subtitle:'im a subtitle',
+    options:[]
+  }
+
+  handleSubmit = (e) =>{
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if(option){
+      this.state.options.push(option);
+      e.target.elements.option.value = ""
+      this.setState((state) => {
+        return {options: state.options};
+      });
+    }
+    console.log(this.state.options)
+  }
+
+  handleClick = () => {
+
+      this.setState((state)=>{
+        return {options: state.options = []}
+      })
+      console.log('reset state',this.state.options)
+  }
+
   render() {
+    const {title,subtitle,options} = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h1>{title}</h1>
+          {subtitle && <p>{subtitle}</p>}
+          <p>{options.length > 0 ? "here are your options" : " no options"}</p>
+          <p>{options.length}</p>
+          <ol>
+            <li>item one</li>
+            <li>item two</li>
+          </ol>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" name="option" />
+            <button>Add Option</button>
+            <button onClick={this.handleClick}>Remove All</button>
+          </form>
       </div>
     );
   }
